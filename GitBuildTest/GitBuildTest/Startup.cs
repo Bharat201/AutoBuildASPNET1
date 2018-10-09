eaeff954ad12927a97f7f9d22d6323593f259f5f
Bharat K <bharat.Kharawade@gmail.com>
2018-10-09 15:56:19 +0100

    First check in for Git Auto build

---
+++ GitBuildTest/GitBuildTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitBuildTest.Startup))]
+namespace GitBuildTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
